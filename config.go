026692c25fda0d727b35d239d70b3b4285a7771f
duggavo <duggavo@proton.me>
2025-01-01 16:18:23 +0100

    update copyright year

--- config.go
+++ config.go
@@ -1,5 +1,5 @@
 /* SuchMiner
- * Copyright 2024 duggavo
+ * Copyright 2025 duggavo
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
